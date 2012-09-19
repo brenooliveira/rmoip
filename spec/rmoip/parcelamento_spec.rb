1d041283365c086a5f7d86839863e139b96f2862
Breno Oliveira <breno26@gmail.com>
2012-09-18 21:16:16 -0300

    arruma os testes do parcelamento

--- spec/rmoip/parcelamento_spec.rb
+++ spec/rmoip/parcelamento_spec.rb
@@ -7,27 +7,27 @@ describe Rmoip::Parcelamento do
   context "Cria uma parcela" do
     let :parcelamento do
       Rmoip::Parcelamento.new do
-        min 5
-        max 10
-        interest 2.99
-        transfer true
+        min "5"
+        max "10"
+        interest "2.99"
+        transfer "true"
       end
     end
 
     it "com valor minimo" do
-      parcelamento.min eq 5
+      parcelamento.min.should eq "5"
     end
 
     it "com o valor maximo" do
-      parcelamento.max eq 10
+      parcelamento.max.should eq "10"
     end
 
     it "com juros" do
-      parcelamento.interest 2.99
+      parcelamento.interest.should eq "2.99"
     end
 
     it "com o repasse" do
-      parcelamento.transfer true
+      parcelamento.transfer.should eq "true"
     end
   end
 end
